f0d8220cd23935023565b7a806feec42a50e555e
Amitk kumar prasad <amitprasad8112@gmail.com>
2026-01-24 08:41:27 +0530

    landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage.jsx'
 export default function App() {
   return (
-    <div>
-      app
-    </div>
+    <>
+    <Router>
+
+
+    <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+      {/* <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+      <Route path='/dashboard' element={<DashboardPage/>}/> */}
+    </Routes>
+    </Router>
+    </>
   )
 }
---
+++ Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+export default function LandingPage() {
+  return (
+    <div className='landingPageContainer'>
+        <nav>
+          <div className='navHeader'>
+            <h2>Apna video Call</h2>
+          </div>
+          <div className='navlist'>
+            <p>Join as Gest</p>
+            <p>Register</p>
+            <div role='button'>Login</div>
+          </div>
+        </nav>
+    </div>
+  )
+}
